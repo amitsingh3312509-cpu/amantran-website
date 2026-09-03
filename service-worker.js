507057f789094070ddad0bf5bface778e3cf2d76
amitsingh3312509-cpu <amitsingh3312509@gmail.com>
2026-09-03 10:18:37 +0530

    Update cache name and add new icon file

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,33 @@
-const CACHE_NAME = "attendance-tracker-v1";
+const CACHE_NAME = "application-generator-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512 (1).png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
